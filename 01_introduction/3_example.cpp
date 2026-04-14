dc295b60c5cb64dd8a6348bf640aae3385772d36
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-04-14 10:04:16 +0530

    fixed introduction folder

---
+++ 01_introduction/3_example.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    cout << "Enter your name ";
+
+    string name;
+
+    cin >> name;
+
+    cout << "my name is " << name;
+
+    return 0;
+}
