c8facfbe6807dd32ae707a71063792f9efaa6946
sarvaiyarutik <sarvaiyarutik18@gmail.com>
2026-04-25 14:18:35 +0530

    employee

--- 06_Inheritance/6.3/Q1.cpp
+++ 06_Inheritance/6.3/Q1.cpp
@@ -47,17 +47,164 @@ public:
         this->bonus = bonus;
     }
 
-    void display() override {
+    void display() override
+    {
 
-        Employee::display();  
+        Employee::display();
 
-        cout<<"Employee Bonus :"<<bonus<<endl;
+        cout << "Employee Bonus :" << bonus << endl;
     }
 };
 
-int
-main()
+class PartTimeEmployee : public Employee
 {
+
+protected:
+    int emp_hours;
+
+public:
+    PartTimeEmployee(int id, string name, int age, int salary, int hours) : Employee(id, name, age, salary)
+    {
+
+        emp_hours = hours;
+    }
+
+    void display() override
+    {
+
+        Employee::display();
+
+        cout << "part time employee work hours" << emp_hours << endl;
+    };
+};
+
+int main()
+{
+
+    Employee *employee[100];
+
+    int choice;
+
+    int count = 0;
+
+    do
+    {
+        cout << "\n Employee Management system" << endl;
+
+        cout << "\n1.Add Employee" << endl;
+        cout << "\n2.Display All Employee" << endl;
+        cout << "\n3.Delete Employee" << endl;
+        cout << "\n4.Exit" << endl;
+
+        cout << "Enter your choice" << endl;
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            int menu;
+
+            cout << "FullTime Employee" << endl;
+            cout << "PartTime Employee" << endl;
+
+            cin >> menu;
+
+            int id, age, salary;
+            string name;
+
+            cout << "\nEnter your Employee ID : " << endl;
+            cin >> id;
+            cin.ignore();
+
+            cout << "\nEnter your Employee Name : " << endl;
+            getline(cin, name);
+
+            cout << "\nEnter your Employee Age : " << endl;
+            cin >> age;
+            cin.ignore();
+
+            cout << "\nEnter your Employee Salary : " << endl;
+            cin >> salary;
+            cin.ignore();
+
+            if (menu == 1)
+            {
+
+                int bonus;
+
+                cout << "\n Enter your Employee Bonus" << endl;
+                cin >> bonus;
+
+                employee[count] = new FullTimeEmployee(id, name, age, salary, bonus);
+
+                count++;
+            }
+
+            else if (menu == 2)
+            {
+
+                int hours;
+
+                cout << "\n Enter Your PartTime Employee" << endl;
+                cin >> hours;
+
+                employee[count] = new PartTimeEmployee(id, name, age, salary, hours);
+
+                count++;
+            }
+        }
+
+        else if (choice == 2)
+        {
+
+            if (count == 0)
+            {
+                cout << "Employee date found" << endl;
+            }
+
+            else
+            {
+
+                for (int i = 0; i < count; i++)
+                {
+                    employee[i]->display();
+                }
+            }
+        }
+
+        else if (choice == 3)
+        {
+
+            int index;
+
+            cout << "index to delete (0-" << count - 1 << "): " << endl;
+            cin >> index;
+
+            if (index >= 0 && index < count)
+            {
+                delete employee[index];
+
+                for (int i = index; i < count - 1; i++)
+                {
+                    employee[i] = employee[i + 1];
+                }
+                count--;
+                cout << "employee data delele" << endl;
+            }
+
+            else
+            {
+                cout << "invailid index to delete " << endl;
+            };
+        };
+
+    } while (choice != 4);
+
+    for (int i = 0; i < count; i++)
+    {
+        delete employee[i];
+
+        cout << "memory free" << endl;
+    }
 
     return 0;
 }
